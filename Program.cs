9fe33d7493b557c1740d2ad96f4dde7da7c917ac
Georg <GeorgDev15@users.noreply.github.com>
2018-01-07 16:02:22 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomethingForGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yo Joe!");
+        }
+    }
+}
